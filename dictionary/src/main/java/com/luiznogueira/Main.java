95f21fee0523146b92d8b2229f2fd024cf49f62a
nogueiraluiz <mnogueira.luizf@gmail.com>
2024-04-20 21:47:12 -0300

    Add initial request sending code

--- dictionary/src/main/java/com/luiznogueira/Main.java
+++ dictionary/src/main/java/com/luiznogueira/Main.java
@@ -1,7 +1,42 @@
 package com.luiznogueira;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+  public static void main(String[] args) {
+    String apiKey = Objects.requireNonNull(System.getenv("KEY"));
+    String url = Objects.requireNonNull(System.getenv("URL"));
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Type desired word:");
+    String input = sc.nextLine();
+    String requestUrl = buildUrl(url, input, apiKey);
+    try {
+      HttpRequest request = HttpRequest.newBuilder()
+          .uri(new URI(requestUrl))
+          .GET()
+          .build();
+      HttpClient client = HttpClient.newHttpClient();
+      HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } finally {
+      sc.close();
     }
+  }
+
+  private static String buildUrl(String url, String input, String apiKey) {
+    return url + input + "?key=" + apiKey;
+  }
 }
